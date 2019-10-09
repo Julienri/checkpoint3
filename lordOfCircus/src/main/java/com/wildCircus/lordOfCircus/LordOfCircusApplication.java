9b84ff870664aa7b0c2e0c36f07bcc929941948b
Julien Risacher <julien.risacher@gmail.com>
2019-10-09 11:22:35 +0200

    addded SpringInitializer

---
+++ lordOfCircus/src/main/java/com/wildCircus/lordOfCircus/LordOfCircusApplication.java
@@ -0,0 +1,13 @@
+package com.wildCircus.lordOfCircus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LordOfCircusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LordOfCircusApplication.class, args);
+	}
+
+}
