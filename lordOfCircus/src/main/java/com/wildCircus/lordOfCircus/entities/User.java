44ea5c176ee99dba2210a36bd4392638ad89dc50
Julien Risacher <julien.risacher@gmail.com>
2019-10-09 14:20:51 +0200

    added img + changed home + userEntity

---
+++ lordOfCircus/src/main/java/com/wildCircus/lordOfCircus/entities/User.java
@@ -0,0 +1,79 @@
+package com.wildCircus.lordOfCircus.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User{
+
+    public User(){
+    }
+
+    public User(String firstname, String lastname, String mail, String password, String type){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.mail = mail;
+        this.password = password;
+        this.type = type;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String mail;
+    private String password;
+    private String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
