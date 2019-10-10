685bcb94a546245e2845043dbec6eaebe30eac56
Julien Risacher <julien.risacher@gmail.com>
2019-10-10 13:47:16 +0200

    added navbar Responsive + changed css + added springSecurity

--- lordOfCircus/src/main/java/com/wildCircus/lordOfCircus/Controllers/LoginController.java
+++ lordOfCircus/src/main/java/com/wildCircus/lordOfCircus/Controllers/LoginController.java
@@ -1,18 +1,18 @@
 package com.wildCircus.lordOfCircus.Controllers;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.ServletException;
+// import javax.servlet.http.HttpServletRequest;
 
 import com.wildCircus.lordOfCircus.entities.User;
 import com.wildCircus.lordOfCircus.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+// import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 // rajouter SpringSécurity
 @Controller
 public class LoginController {
@@ -20,8 +20,8 @@ public class LoginController {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
+    // @Autowired
+    // PasswordEncoder passwordEncoder;
 
     @GetMapping("/login")
     public String login(){
@@ -29,27 +29,27 @@ public class LoginController {
     }
     
     @GetMapping("/subscribe")
-    public String subscribe(Model model){
-        model.addAttribute("user", new User());
+    public String subscribe()/* (Model model) */{
+        // model.addAttribute("user", new User());
         return "subscribe";
     }
 
-    @PostMapping("/subscribe")
-    public String store(HttpServletRequest request, String fistname, 
-        String lastname, String mail, String password){
-            @RequestParam(defaultValue = "false"){
-                User.setPassword(passwordEncoder.encode(user.getPassword()));
-                user = userRepository.save(user);
-                try{
-                    request.login(mail,password);
-                }
-                catch(ServletException e){
-                    e.printStackTrace();
-                }
-            }
-        return "redirect:/";
-    }
-    // Si page de profil ajouter ça !
+    // @PostMapping("/subscribe")
+    // public String store(HttpServletRequest request, String fistname, 
+    //     String lastname, String mail, String password){
+    //         @RequestParam(defaultValue = "false"){
+    //             User.setPassword(passwordEncoder.encode(user.getPassword()));
+    //             user = userRepository.save(user);
+    //             try{
+    //                 request.login(mail,password);
+    //             }
+    //             catch(ServletException e){
+    //                 e.printStackTrace();
+    //             }
+    //         }
+    //     return "redirect:/";
+    // }
+    // Si page de profil ajouter
     // @GetMapping("/profile")
     // public String read(Model model, Authentication authentication, int id){
     //     User user = userRepository.findByMail(authentication.getName());
---
+++ lordOfCircus/src/main/java/com/wildCircus/lordOfCircus/config/MySecurityConfig.java
@@ -0,0 +1,23 @@
+package com.wildCircus.lordOfCircus.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter{
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+            .authorizeRequests()
+                .antMatchers("/img/**").permitAll()
+                .antMatchers("/**.css").permitAll()
+                .antMatchers("/**").permitAll();
+    }
+
+}
