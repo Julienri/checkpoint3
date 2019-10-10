f7882a2c076e3701222b5da213b4ca4b080a09d5
Julien Risacher <julien.risacher@gmail.com>
2019-10-10 11:26:35 +0200

    added springsecurity

--- lordOfCircus/src/main/java/com/wildCircus/lordOfCircus/Controllers/LoginController.java
+++ lordOfCircus/src/main/java/com/wildCircus/lordOfCircus/Controllers/LoginController.java
@@ -7,6 +7,7 @@ import com.wildCircus.lordOfCircus.entities.User;
 import com.wildCircus.lordOfCircus.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
