d564007d2992c36fd0f801bbc288f39292edd2a9
Julien Risacher <julien.risacher@gmail.com>
2019-10-14 09:54:33 +0200

    added

--- lordOfCircus/src/main/java/com/wildCircus/lordOfCircus/config/MySecurityConfig.java
+++ lordOfCircus/src/main/java/com/wildCircus/lordOfCircus/config/MySecurityConfig.java
@@ -4,18 +4,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class MySecurityConfig extends WebSecurityConfigurerAdapter{
-    
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
             .authorizeRequests()
                 .antMatchers("/img/**").permitAll()
                 .antMatchers("/**.css").permitAll()
-                .antMatchers("/**").permitAll();
+                .antMatchers("/**").permitAll()
+            .and()
+            .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/profile", true)
+                .permitAll()
+                .failureUrl("/loginError")
+                .permitAll()
+            .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login");
     }
-
 }
